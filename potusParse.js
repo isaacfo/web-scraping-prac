941b2e36f1a34a9bfc8378c19d1d599551d87ea9
Isaac Fonseca <isaacfo1010@gmail.com>
2019-01-02 15:06:12 -0500

    added file to render g wash page

---
+++ potusParse.js
@@ -0,0 +1,11 @@
+
+const rp = require('request-promise');
+const url = 'https://en.wikipedia.org/wiki/George_Washington';
+
+rp(url)
+  .then(function(html) {
+    console.log(html);
+  })
+  .catch(function(err) {
+    //handle error
+  });
